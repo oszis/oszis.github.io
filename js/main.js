a1547e3810dec7514d6ef7a0c9cdf528fca7b5b6
oszis <belkin643@gmail.com>
2017-05-02 04:43:01 +0700

    new wersion 0.0.3

--- js/main.js
+++ js/main.js
@@ -1,78 +1,160 @@
 (function($){
-
-	// меню на мобилках по клику
-	$(document).on('click', function (e) {
-		var hamburger = $('.hamburger'),
-			nav = $('.mobile-header__nav');
-		if ($(e.target).closest('.hamburger').length) {
-
-			nav.slideToggle('fast');
-			hamburger.toggleClass('active');
-		} else if (hamburger.hasClass('active')) {
-			nav.slideUp('fast');
-			hamburger.removeClass('active');
-		}
-	});
-
-	// добавляем класс active элементу списка nav
-	function navLinkActive(e) {
-		$('.mobile-header__nav a, .main-header__nav a').removeClass('active');
-		e.addClass('active');
-	}
-
-	// скролл к section по клику на ссылку в навигации
-	$('.mobile-header__nav a, .main-header__nav a').click(function(e){
-		e.stopPropagation();
-		var link = $(this);
-		navLinkActive(link);
-		var elemHref = link.attr('href');
-		$('html, body').clearQueue();
-		$('html, body').animate({
-			scrollTop: $(elemHref).offset().top
-		}, 800);
-	});
+	// Сюда код
 
 
-	// tooltip
-	var tip = {
-		show: function(){
-			var elem = $(this),
-				elemAttr = elem.attr('data-name'),
-				tip = '<div class="tip">' + elemAttr + '</div>';
-			if ($('body').children('.tip').length == 0) {
-				$('body').append(tip);
-				var width = $('.tip').css('width');
-				$('.tip').css({
-					'top': elem.offset().top - $('.tip').outerHeight() - 10 + 'px',
-					'left': elem.offset().left + elem.outerWidth() /2 -  $('.tip').outerWidth()/2 + 'px',
-				});
-			} else return false;
-		},
-		hide: function(){
-			$('.tip').remove();
-		}
-	}
+	// скролл страницы:
 
-	$('.btn-tip').hover(tip.show, tip.hide);
+	// если блок контента больше, чем высота окна, то:
 
-	// Меняем background у fixed header по scroll
+	// 1. скроллим до конца блока
 
-	var scrollSection = $('.scroll-section');
-	$(window).on('scroll load', function(){
-		scrollSection.each(function(){
-			var elem = $(this),
-				scrolled = $(window).scrollTop(),
-				header = $('.main-header');
-			if (scrolled >= elem.offset().top && scrolled <= elem.offset().top + elem.height()) {
-				switch (true) {
-					case elem.hasClass('scroll-section--grey'):
-						header.removeClass('negative');
-						break;
-					case elem.hasClass('scroll-section--white'):
-						header.addClass('negative')
-						break;
-				}
+	// 2. скроллим к следующему блоку
+
+	function SlideScroll(){
+
+		var _this = this,
+			activeClass = 'active',
+			lastClass = 'last',
+			scrollValue = 0,
+			slides,
+			activeSlide;
+
+		this.scrollTimeout = true;
+
+		this.scrollDetect = function(e){
+			
+			e = e || window.event;
+			var delta = e.deltaY || e.detail || e.wheelDelta;
+
+			scrollValue = delta;
+
+			if (scrollValue > 0) {
+				console.log('Событие колесика сработало!');
+				return _this.slideNext(activeSlide);
+			} else if (scrollValue < 0) {
+				return _this.slidePrev(activeSlide);
 			}
+		}
+
+		this.slidePrev = function(){
+			
+			if (activeSlide > 0) {
+				
+				_this.slideHide(slides.eq(activeSlide));
+
+				activeSlide--;
+
+				_this.slideShow(slides.eq(activeSlide));
+
+			} else return false;
+		}
+
+		this.slideNext = function(){
+
+			
+			console.log('Прокрутка!');
+			console.log(slides.length);
+			if (activeSlide < slides.length - 1) {
+				
+				_this.slideHide(slides.eq(activeSlide));
+
+
+				activeSlide++;
+
+				_this.slideShow(slides.eq(activeSlide));
+
+				console.log(activeSlide);
+			} else return false;
+
+		}
+
+		this.slideShow = function(slide){
+			slide.removeClass(lastClass);
+			slide.addClass(activeClass);
+		}
+
+		this.slideHide = function(slide){
+			slide.removeClass(activeClass);
+			slide.addClass(lastClass);
+		}
+
+		this.initSlider = function(elem){
+
+			slides = elem;
+
+			activeSlide = 0;
+
+			slides.eq(activeSlide).addClass(activeClass);
+
+		}
+
+		this.init = function(obj){
+
+			_this.initSlider(obj.blocks);
+
+		}
+
+	}
+
+	var slideScrollVar = new SlideScroll();
+
+	slideScrollVar.init({
+		blocks: $('.main-section'),
+	})
+
+
+	document.querySelectorAll('.page')[0]
+		.addEventListener('wheel', function(){
+			if (slideScrollVar.scrollTimeout) {
+				slideScrollVar.scrollDetect();
+				slideScrollVar.scrollTimeout = false;
+				setTimeout(function(){
+					slideScrollVar.scrollTimeout = true;
+				}, 1000);
+			} else {
+				return false;
+			}
+
 		});
+
+
+
+function MainMenu(){
+
+	var _this = this,
+		activeClass = 'active',
+		headerElem;
+
+	_this.showMenu = function(header){
+		headerElem.addClass(activeClass);
+	}
+
+	_this.hideMenu = function(header){
+		headerElem.removeClass(activeClass);
+	}
+
+	_this.init = function(obj){
+		headerElem = obj.block;
+	}
+}
+
+	var mainMenuVar = new MainMenu();
+
+	mainMenuVar.init({
+		block: $('.main-header')
 	});
+
+	$('.btn-show-menu').click(function(){
+
+		var elem = $(this);
+		if (elem.hasClass('active')) {
+			$('.btn-show-menu').removeClass('active');
+
+			mainMenuVar.hideMenu();
+		} else {
+			$('.btn-show-menu').addClass('active');
+			mainMenuVar.showMenu();
+		}
+	})
+
 })(jQuery);
