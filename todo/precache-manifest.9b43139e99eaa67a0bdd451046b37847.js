e3519af76ada9b9b89cccbddad7c036b72314d49
Bruz <belkin643@gmail.com>
2019-03-19 21:10:59 +0700

    #master add todo

---
+++ todo/precache-manifest.9b43139e99eaa67a0bdd451046b37847.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "179fa99de3f3c8e0cf83",
+    "url": "/static/js/main.f5b8fa5b.chunk.js"
+  },
+  {
+    "revision": "64ece872a1d8a8c7a890",
+    "url": "/static/js/2.0ae0fc90.chunk.js"
+  },
+  {
+    "revision": "179fa99de3f3c8e0cf83",
+    "url": "/static/css/main.ecd143a0.chunk.css"
+  },
+  {
+    "revision": "9c7d35032248f891d702e9cde6dfb574",
+    "url": "/index.html"
+  }
+];
