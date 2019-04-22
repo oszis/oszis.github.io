61d56e23df8b8151c8069a062134caed7322314e
Bruz <belkin643@gmail.com>
2019-04-22 07:34:02 +0700

    #master sample-game

---
+++ sample-game/precache-manifest.78243f8343852e693cd2b783d1c9b7f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e82de622479489f11b08",
+    "url": "/static/js/main.954f39ea.chunk.js"
+  },
+  {
+    "revision": "c988897debcc57986854",
+    "url": "/static/js/2.21110c12.chunk.js"
+  },
+  {
+    "revision": "e82de622479489f11b08",
+    "url": "/static/css/main.c02cc3bc.chunk.css"
+  },
+  {
+    "revision": "c988897debcc57986854",
+    "url": "/static/css/2.cb10c6a6.chunk.css"
+  },
+  {
+    "revision": "cff54f78e3032bef7fb117e7fa7aa076",
+    "url": "/index.html"
+  }
+];
